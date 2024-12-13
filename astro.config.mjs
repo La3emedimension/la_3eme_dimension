41aaaafeca576d670c86e45976befa3d26c22af8
La3emedimension <la3emedim@gmail.com>
2024-12-13 23:05:39 +0100

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://github.com/La3emedimension',
-  base: 'https://La3emedimension.github.io/la3emedimension ',
+  base: 'https://La3emedimension.github.io/la_3eme_dimension ',
 })
