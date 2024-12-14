99cd641f7739246c908aecbef787a942b59d6775
La3emedimension <la3emedim@gmail.com>
2024-12-14 09:00:48 +0100

    Base upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://github.com/La3emedimension',
-  base: 'https://La3emedimension.github.io/la3emedimension ',
+  site: 'https://La3emedimension.github.io/',
+  base: '/la3emedimension ',
 })
