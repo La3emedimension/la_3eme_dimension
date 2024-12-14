f3d2c32c86ac6c526c247e1267c74e6124415aa1
La3emedimension <la3emedim@gmail.com>
2024-12-14 09:05:37 +0100

    undescore upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://La3emedimension.github.io/',
-  base: '/la3emedimension ',
+  base: '/la_3eme_dimension ',
 })
