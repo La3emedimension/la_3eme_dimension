c587782bec0ae50f42475ebc440d25115a1d5399
la3emedimension <la3emedim@gmail.com>
2026-09-04 15:36:45 +0200

    DEPLOY

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ export default defineConfig({
   base: '/',
   redirects: {
     '/product/ert': '/product/lumi/',
+    '/en/blog/led-sign': '/en/blog/enseigne-lumineuse/',
+    '/en/blog/servicehelper': '/en/blog/potcedric/',
   },
   integrations: [
     sitemap({
